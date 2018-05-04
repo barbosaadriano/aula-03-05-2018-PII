f765226095619ba1ba72a9b982d17fa6dac3343a
Adriano Barbosa <b.adrianobarbosa@gmail.com>
2018-05-03 22:21:17 -0300

    Hello world

---
+++ src/br/com/adrianob/HelloWorldV2.java
@@ -0,0 +1,14 @@
+package br.com.adrianob;
+
+import java.util.Random;
+
+
+public class HelloWorldV2 {
+
+    public static void main(String[] args) {
+        int numero = 0;
+        Random ram = new Random();
+        int meuIntAleatorio = ram.nextInt();
+        System.out.println("Hello World");
+    }
+}
